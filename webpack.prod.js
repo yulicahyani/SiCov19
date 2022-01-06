2ab02c8e87f730419c1588176bf480145cd2e3b8
Ni Made Yuli Cahyani <71582007+yulicahyani@users.noreply.github.com>
2022-01-06 16:04:38 +0800

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    mode : 'production',
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : '/node_modules/',
+                use : [
+                    {
+                        loader : 'babel-loader',
+                        options : {
+                            presets : ['@babel/preset-env']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template : './src/index.html',
+            filename : 'index.html',
+        })
+    ]
+});
